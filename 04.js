c93e1bb2a79d12748cd47cf5ace8c282393b4d67
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2022-04-02 20:45:56 +0900

    Feat : 여러가지 변수 선언 방법

---
+++ 04.js
@@ -0,0 +1,21 @@
+var userId = 1;
+var userName = 'Lee';
+
+var user = { id : 1, name : 'Lee'};
+
+var users = [
+    { id : 1, name : 'Lee'},
+    { id : 2, name : 'Kim'}
+];
+
+var result = 10 + 20
+result = 40
+
+var boolean = true
+
+console.log(boolean);
+console.log(result);
+console.log(user.name);
+console.log(users[0].id);
+console.log(users[0].name);
+
