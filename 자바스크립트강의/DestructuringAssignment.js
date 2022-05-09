fcc94080c9526834a007c81cb9cbe0813451bb96
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2022-05-09 21:00:42 +0900

    Feat : 구조분해 할당

--- 자바스크립트강의/DestructuringAssignment.js
+++ 자바스크립트강의/DestructuringAssignment.js
@@ -4,6 +4,21 @@ const obj = {
     b: 2,
 }
 
-const { a, b } = obj;
-console.log(a);
-console.log(b);
+const { a1, b1 } = obj;
+console.log(a1);
+console.log(b1);
+
+// a, c, e 구조분해 할당하기
+const quizObj = {
+    a: 'hello',
+    b: {
+        c: history,
+        d: { e: 'wow' },
+    },
+};
+
+// 구조분해 할당할 때 b: {} 이렇게 객체선언하면 변수로 사용불가능하다.
+const { a, b: { c, d: {e} } } = quizObj;
+// const a = obj.a;
+// const c = obj.b.c;
+// const e = obj.b.d.e;
